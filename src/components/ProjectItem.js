bfa5a14285eb6cb907cadbaeecd30d2bc4bdb7ab
si0107 <sarikaislam.02@gmail.com>
2022-07-11 01:33:58 -0400

    Updated Project Item

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -23,13 +23,23 @@ function ProjectItem(props) {
   return (
     <div className='ProjectItem'>
         <div className='ProjectItem-container'>
-            <div className='coffee'><img src={props.content[0]}/></div>
+            <div className='coffee'>
+                <img src={props.content[0]}/>
+            </div>
             <div className='project-card'>
-                <div className='project-picture'>
-                    {togglePic ? <img onClick= {toggleProjPic} src={props.content[1]}/> : <img onClick= {toggleProjPic} src={props.content[2]}/>}
+                <div className='project-card-content'>
+                    <div className='project-picture'>
+                        {togglePic ? 
+                        <img id= 'projpic1' onClick= {toggleProjPic} src={props.content[1]}/> : 
+                        <img id= 'HTcoffee' onClick= {toggleProjPic} src={props.content[2]}/>}
+                    </div>
+
+                    <div className='project-card-text'>
+                        <div className='project-description'>{props.content[3]}</div>
+                        <div className='project-lang'>{props.content[4]}</div>
+                    </div>
                 </div>
-                <div className='project-description'>{props.content[3]}</div>
-                <div className='project-lang'>{props.content[4]}</div>
+                
                 <div className='project-link'>
                     <a href= {props.content[5]} target='_blank'>
                         <img src={props.content[6]}></img>
